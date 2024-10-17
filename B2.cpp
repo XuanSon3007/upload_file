327ec8188388daff2d68bed9356465d0c17ef7cb
sonnguyen <khonglido3007@gmail.com>
2024-10-17 16:27:43 +0700

    assigment17/10

---
+++ B2.cpp
@@ -0,0 +1,140 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+
+using namespace std;
+
+// hàm kiểm tra số nguyên tố
+
+bool checkPrime(int n){
+    if(n<2) return false;
+    for(int i=2;i<sqrt(n);i++){
+        if(n%i==0){
+            return false;
+        } 
+        }
+        return true;
+    }
+
+    //Tạo mảng chứa số nửa nguyên tố blum nhỏ hơn N
+
+    vector<int> array_blumNumbers(int N){
+        vector<int> primes;
+        vector<int> blumNumbers;
+
+        //Tìm các số nguyên tố có dạng 4k+3
+        for(int i=0;i<N;i++){
+            if( checkPrime(i) && i%4==3){
+             primes.push_back(i);   
+    
+            }
+
+            }
+
+    //Taọ số nguyên tố blum từ các số nguyên tố trên
+
+    for(int i=0;i<primes.size();i++){
+        for(int j=i;j<primes.size();j++){
+            int blum=primes[i]*primes[j];
+            if(blum<N){
+            //Kiểm tra xem blum đã tồn tại trong blumNumbers hay chưa
+            bool exist=false;
+            for(int k=0;k<blumNumbers.size();k++){
+                if(blumNumbers[k]==blum) {
+                exist=true;
+                break;
+                }
+            }
+            if(!exist){
+                blumNumbers.push_back(blum);
+            }   
+            }
+        }
+    }
+
+    //Săp xếp blumNumbers
+
+    for(int i=0; i<blumNumbers.size()-1; i++){
+        for(int j=i+1; j<blumNumbers.size(); j++){
+            if(blumNumbers[i]>blumNumbers[j]){
+                swap(blumNumbers[i], blumNumbers[j]);
+    
+        }
+    }
+    }
+    return blumNumbers;
+    }
+    // 1.Tìm các cặp số blum có tổng cũng là 1 số blum nhỏ hơn N
+
+    vector<pair<int,int>> findBlum( vector<int> &blumNumbers, int N){
+        vector<pair<int,int>> pairs;
+        for(int i=0;i<blumNumbers.size();i++){
+            for(int j=i+1;j<blumNumbers.size();j++){
+                int sum=blumNumbers[i]+blumNumbers[j];
+                //Tìm kiếm xem sum có nằm trong blumNumbẻrs không
+                bool exist=false;
+                for(int k=0;k<blumNumbers.size();k++){
+                    if(blumNumbers[k]==sum){
+                        exist=true;
+                        break;
+                    }
+                }
+                if(sum<N && exist){
+                    pairs.push_back({blumNumbers[i], blumNumbers[j]});
+        } 
+
+    }
+        }
+        return pairs;
+    }
+
+
+    //2. Kiểm tra xem 1 số blum M có tồn tại hay khôg
+
+    bool checkBlum(int M, const vector<int> &blumNumbers){
+        for(int i=0; i<blumNumbers.size();i++){
+            if(blumNumbers[i]==M){
+                return true;
+            }
+    
+            }
+            return false;
+        }
+
+        int main(){
+            int N,M;
+            cin>>N;
+
+            //Tạo mảng số nửa nguyên tố blum nhỏ hơn N
+            vector<int> blumNumbers=array_blumNumbers(N);
+
+            //In ra các số blum nhỏ hơn N
+            cout<<"Cac so blum nho hon "<<N<<":";
+            for(int i=0;i<blumNumbers.size();i++){
+                
+                cout<<blumNumbers[i]<<" ";
+            }
+            cout<<endl;
+
+            //In ra các căp có tổng cũng là số blum nhỏ hơn N
+
+            cout<<"Cac cap so blum co tong nho hon "<<N<< ":";
+            vector<pair<int,int>> blumPairs=findBlum(blumNumbers,N);
+            for(int i=0;i<blumPairs.size();i++){
+                cout<<"("<<blumPairs[i].first<<","<<blumPairs[i].second<<") ";
+
+            }
+         
+
+            // Kiểm tra số blum M
+
+            cin>>M;
+            if(checkBlum(M,blumNumbers)){
+                cout<<"M co ton tai trong day so blum ";
+            }else {
+                cout<<"M khong ton tai trong day so blum ";
+            }
+            return 0;
+            
+        }
+    
